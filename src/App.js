57e8245ba8d907e1843679da1c72908e3f71c674
Max Zakharzhevskiy <max.zakh@cyphereye.com>
2020-11-28 21:50:50 -0800

    fix: remove findDOMNode warning

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import * as React from 'react';
-import { Admin } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import './App.css';
+
+(function () {
+  const oldConsoleError = console.error;
+  console.error = function (...args) {
+    const isOurWarning = typeof args[3] === 'string' && args[3] === 'Transition' &&
+      typeof args[1] === 'string' && args[1] === 'findDOMNode' &&
+      typeof args[0] === 'string' && args[0].indexOf('is deprecated in StrictMode.') >= -1;
+
+    if (!isOurWarning) {
+      oldConsoleError(...args);
+    }
+  };
+}())
 
 const dataProdiver = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 function App() {
   return (
-    <Admin dataProvider={dataProdiver} />
+    <Admin dataProvider={dataProdiver}>
+      <Resource name="users" list={ListGuesser} />
+    </Admin>
   );
 }
 
